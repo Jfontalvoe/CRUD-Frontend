37e1c1c1a3a270f59d10794b64a6a8beef3f5d19
William Perez <73206273+wil2611@users.noreply.github.com>
2023-11-24 16:02:07 -0500

    cambios

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var newMemberAddBtn = document.querySelector('.Registrar'),
-darkBg = document.querySelector('.dark_bg'),
-popupForm = document.querySelector('.popup'),
-crossBtn = document.querySelector('.closeBtn'),
-submitBtn = document.querySelector('.submitBtn'),
- modalTitle = document.querySelector('.modalTitle'),
- popupFooter = document.querySelector('.popupFooter'),
- imgInput = document.querySelector('.img'),
- imgHolder = document.querySelector('.imgholder')
- form = document.querySelector('form'),
- formInputFields = document.querySelectorAll('form input'),
-  uploadimg = document.querySelector("#uploadimg"),
-  tipoDNI = document.getElementById("tipoDNI"),
-  no_documento = document.getElementById("no_documento"),
-  fName = document.getElementById("fName"),
-  sName = document.getElementById("sName"),
-  genero = document.getElementById("genero"),
-  lName = document.getElementById("lName"),
-  bDate = document.getElementById("bDate"),
-  email = document.getElementById("email"),
-  phone = document.getElementById("phone"),
-  entries = document.querySelector(".showEntries"),
-  tabSize = document.getElementById("table_size"),
-  userInfo = document.querySelector(".userInfo"),
-  table = document.querySelector("table"),
-  filterData = document.getElementById("search")
+var newMemberAddBtn = document.querySelector('.addMemberBt button.addMemberBtn'),
+    darkBg = document.querySelector('.dark_bg'),
+    popupForm = document.querySelector('.popup'),
+    crossBtn = document.querySelector('.closeBtn'),
+    submitBtn = document.querySelector('.submitBtn'),
+    modalTitle = document.querySelector('.modalTitle'),
+    popupFooter = document.querySelector('.popupFooter'),
+    imgInput = document.querySelector('.img'),
+    imgHolder = document.querySelector('.imgholder')
+form = document.querySelector('form'),
+    formInputFields = document.querySelectorAll('form input'),
+    uploadimg = document.querySelector("#uploadimg"),
+    tipoDNI = document.getElementById("tipoDNI"),
+    no_documento = document.getElementById("no_documento"),
+    fName = document.getElementById("fName"),
+    sName = document.getElementById("sName"),
+    genero = document.getElementById("genero"),
+    lName = document.getElementById("lName"),
+    bDate = document.getElementById("bDate"),
+    email = document.getElementById("email"),
+    phone = document.getElementById("phone"),
+    entries = document.querySelector(".showEntries"),
+    tabSize = document.getElementById("table_size"),
+    userInfo = document.querySelector(".userInfo"),
+    table = document.querySelector("table"),
+    filterData = document.getElementById("search")
 
 let originalData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
 let getData = [...originalData]
@@ -41,7 +41,7 @@ var maxIndex = 0
 showInfo()
 
 
-newMemberAddBtn.addEventListener('click', ()=> {
+newMemberAddBtn.addEventListener('click', () => {
     isEdit = false
     submitBtn.innerHTML = "Registrar"
     modalTitle.innerHTML = "Registrar persona"
@@ -51,49 +51,49 @@ newMemberAddBtn.addEventListener('click', ()=> {
     popupForm.classList.add('active')
 })
 
-crossBtn.addEventListener('click', ()=>{
+crossBtn.addEventListener('click', () => {
     darkBg.classList.remove('active')
     popupForm.classList.remove('active')
     form.reset()
 })
 
 
-uploadimg.onchange = function(){
+uploadimg.onchange = function () {
     var selectedFile = uploadimg.files[0];
-    if(selectedFile && /^image\//.test(selectedFile.type) && selectedFile.size <= 2000000){   // 2MB = 2000000
+    if (selectedFile && /^image\//.test(selectedFile.type) && selectedFile.size <= 2000000) {   // 2MB = 2000000
         var fileReader = new FileReader()
 
-        fileReader.onload = function(e){
+        fileReader.onload = function (e) {
             var imgUrl = e.target.result
             imgInput.src = imgUrl
         }
 
         fileReader.readAsDataURL(uploadimg.files[0])
-    } 
-    else if(!/^image\//.test(selectedFile.type)){
+    }
+    else if (!/^image\//.test(selectedFile.type)) {
         alert("El archivo debe tener formato de imagen")
-    } 
-    else if(selectedFile.size > 2000000){
+    }
+    else if (selectedFile.size > 2000000) {
         alert("Esta foto es muy grande, recuerde que deber ser menor a 2MB")
     }
-    else if(!selectedFile){
+    else if (!selectedFile) {
         alert("Por favor anexar la foto")
     }
 }
 
-function preLoadCalculations(){
+function preLoadCalculations() {
     array = getData
     arrayLength = array.length
     maxIndex = arrayLength / tableSize
 
-    if((arrayLength % tableSize) > 0){
+    if ((arrayLength % tableSize) > 0) {
         maxIndex++
     }
 }
 
 
 
-function displayIndexBtn(){
+function displayIndexBtn() {
     preLoadCalculations()
 
     const pagination = document.querySelector('.pagination')
@@ -102,8 +102,8 @@ function displayIndexBtn(){
 
     pagination.innerHTML = '<button onclick="prev()" class="prev">Previous</button>'
 
-    for(let i=1; i<=maxIndex; i++){
-        pagination.innerHTML += '<button onclick= "paginationBtn('+i+')" index="'+i+'">'+i+'</button>'
+    for (let i = 1; i <= maxIndex; i++) {
+        pagination.innerHTML += '<button onclick= "paginationBtn(' + i + ')" index="' + i + '">' + i + '</button>'
     }
 
     pagination.innerHTML += '<button onclick="next()" class="next">Next</button>'
@@ -112,15 +112,15 @@ function displayIndexBtn(){
 }
 
 
-function highlightIndexBtn(){
+function highlightIndexBtn() {
     startIndex = ((currentIndex - 1) * tableSize) + 1
     endIndex = (startIndex + tableSize) - 1
 
-    if(endIndex > arrayLength){
+    if (endIndex > arrayLength) {
         endIndex = arrayLength
     }
 
-    if(maxIndex >= 2){
+    if (maxIndex >= 2) {
         var nextBtn = document.querySelector(".next")
         nextBtn.classList.add("act")
     }
@@ -131,7 +131,7 @@ function highlightIndexBtn(){
     var paginationBtns = document.querySelectorAll('.pagination button')
     paginationBtns.forEach(btn => {
         btn.classList.remove('active')
-        if(btn.getAttribute('index') === currentIndex.toString()){
+        if (btn.getAttribute('index') === currentIndex.toString()) {
             btn.classList.add('active')
         }
     })
@@ -143,20 +143,20 @@ function highlightIndexBtn(){
 
 
 
-function showInfo(){
+function showInfo() {
     document.querySelectorAll(".employeeDetails").forEach(info => info.remove())
 
     var tab_start = startIndex - 1
     var tab_end = endIndex
 
-    if(getData.length > 0){
-        for(var i=tab_start; i<tab_end; i++){
+    if (getData.length > 0) {
+        for (var i = tab_start; i < tab_end; i++) {
             var staff = getData[i]
 
 
-            if(staff){
+            if (staff) {
                 let createElement = `<tr class = "employeeDetails">
-                <td>${i+1}</td>
+                <td>${i + 1}</td>
                 <td><img src="${staff.picture}" alt="" width="40" height="40"></td>
                 <td>${staff.tipoDNIVal}</td>
                 <td>${staff.no_documentoVal}</td>
@@ -184,7 +184,7 @@ function showInfo(){
     }
 
 
-    else{
+    else {
         userInfo.innerHTML = `<tr class="employeeDetails"><td class="empty" colspan="11" align="center">No data available in table</td></tr>`
         table.style.minWidth = "1400px"
     }
@@ -193,7 +193,7 @@ function showInfo(){
 showInfo()
 
 // Leer información en el ojito
-function readInfo(pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate, Email, Phone){
+function readInfo(pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate, Email, Phone) {
     imgInput.src = pic
     tipoDNI.value = TipoDNI
     no_documento.value = No_documento
@@ -211,9 +211,10 @@ function readInfo(pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate
     modalTitle.innerHTML = "Perfil"
     formInputFields.forEach(input => {
         input.disabled = true;
-        submitBtn.disabled = true;
+
     });
-    
+    submitBtn.disabled = true;
+
 
 
     imgHolder.style.pointerEvents = "none"
@@ -221,7 +222,7 @@ function readInfo(pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate
 
 
 // Abrir editar información 
-function editInfo(id, pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate, Email, Phone){
+function editInfo(id, pic, TipoDNI, No_documento, fname, sname, lname, Genero, BDate, Email, Phone) {
     isEdit = true
     editId = id
 
@@ -263,7 +264,7 @@ function editInfo(id, pic, TipoDNI, No_documento, fname, sname, lname, Genero, B
     formInputFields.forEach(input => {
         input.disabled = false
     })
-
+    submitBtn.disabled = false;
 
     imgHolder.style.pointerEvents = "auto"
     submitBtn.addEventListener('click', handleUpdate);
@@ -271,89 +272,9 @@ function editInfo(id, pic, TipoDNI, No_documento, fname, sname, lname, Genero, B
 
 // Función para manejar la actualización
 function handleUpdate() {
-const information = {
-    id: Date.now(),
-    picture: imgInput.src == undefined ? "./img/pic1.png" :imgInput.src,
-    tipoDNIVal: tipoDNI.value,
-    no_documentoVal: no_documento.value,
-    fName: fName.value,
-    sName: sName.value,
-    lName: lName.value,
-    generoVal: genero.value,
-    bDateVal: bDate.value,
-    emailVal: email.value,
-    phoneVal: phone.value
-}
-
-if(!isEdit){
-    originalData.unshift(information)
-}
-else{
-    originalData[editId] = information
-}
-getData = [...originalData]
-localStorage.setItem('userProfile', JSON.stringify(originalData))
-
-darkBg.classList.remove('active')
-popupForm.classList.remove('active')
-form.reset()
-
-
-highlightIndexBtn()
-displayIndexBtn()
-showInfo()
-
-// Eliminar el controlador de eventos para evitar múltiples asignaciones
-submitBtn.removeEventListener('click', handleUpdate);
-}
-// Eliminar datos
-function deleteInfo(index){
-    if(confirm("Estás seguro que quieres eliminar este registro?")){
-        originalData.splice(index, 1);
-        localStorage.setItem("userProfile", JSON.stringify(originalData));
-        
-        // Update getData after deleting the record
-        getData = [...originalData];
-
-        preLoadCalculations()
-
-        if(getData.length === 0){
-            currentIndex = 1
-            startIndex = 1
-            endIndex = 0
-        }
-        else if(currentIndex > maxIndex){
-            currentIndex = maxIndex
-        }
-
-        showInfo()
-        highlightIndexBtn()
-        displayIndexBtn()
-
-        var nextBtn = document.querySelector('.next')
-        var prevBtn = document.querySelector('.prev')
-
-        if(Math.floor(maxIndex) > currentIndex){
-            nextBtn.classList.add("act")
-        }
-        else{
-            nextBtn.classList.remove("act")
-        }
-
-
-        if(currentIndex > 1){
-            prevBtn.classList.add('act')
-        }
-    }
-}
-
-// Registrar en el form
-form.addEventListener('submit', (e)=> {
-    e.preventDefault()
-
     const information = {
         id: Date.now(),
-        picture: imgInput.src == undefined ? "./img/pic1.png" :imgInput.src,
+        picture: imgInput.src == undefined ? "./img/pic1.png" : imgInput.src,
         tipoDNIVal: tipoDNI.value,
         no_documentoVal: no_documento.value,
         fName: fName.value,
@@ -365,10 +286,89 @@ form.addEventListener('submit', (e)=> {
         phoneVal: phone.value
     }
 
-    if(!isEdit){
+    if (!isEdit) {
         originalData.unshift(information)
     }
-    else{
+    else {
+        originalData[editId] = information
+    }
+    getData = [...originalData]
+    localStorage.setItem('userProfile', JSON.stringify(originalData))
+
+    darkBg.classList.remove('active')
+    popupForm.classList.remove('active')
+    form.reset()
+
+
+    highlightIndexBtn()
+    displayIndexBtn()
+    showInfo()
+
+    // Eliminar el controlador de eventos para evitar múltiples asignaciones
+    submitBtn.removeEventListener('click', handleUpdate);
+}
+// Eliminar datos
+function deleteInfo(index) {
+    if (confirm("Estás seguro que quieres eliminar este registro?")) {
+        originalData.splice(index, 1);
+        localStorage.setItem("userProfile", JSON.stringify(originalData));
+
+        // Update getData after deleting the record
+        getData = [...originalData];
+
+        preLoadCalculations()
+
+        if (getData.length === 0) {
+            currentIndex = 1
+            startIndex = 1
+            endIndex = 0
+        }
+        else if (currentIndex > maxIndex) {
+            currentIndex = maxIndex
+        }
+
+        showInfo()
+        highlightIndexBtn()
+        displayIndexBtn()
+
+        var nextBtn = document.querySelector('.next')
+        var prevBtn = document.querySelector('.prev')
+
+        if (Math.floor(maxIndex) > currentIndex) {
+            nextBtn.classList.add("act")
+        }
+        else {
+            nextBtn.classList.remove("act")
+        }
+
+
+        if (currentIndex > 1) {
+            prevBtn.classList.add('act')
+        }
+    }
+}
+
+// Registrar en el form
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const information = {
+        id: Date.now(),
+        picture: imgInput.src == undefined ? "./img/pic1.png" : imgInput.src,
+        tipoDNIVal: tipoDNI.value,
+        no_documentoVal: no_documento.value,
+        fName: fName.value,
+        sName: sName.value,
+        lName: lName.value,
+        generoVal: genero.value,
+        bDateVal: bDate.value,
+        emailVal: email.value,
+        phoneVal: phone.value
+    }
+    if (!isEdit) {
+        originalData.unshift(information)
+    }
+    else {
         originalData[editId] = information
     }
     getData = [...originalData]
@@ -388,53 +388,53 @@ form.addEventListener('submit', (e)=> {
 
     var nextBtn = document.querySelector(".next")
     var prevBtn = document.querySelector(".prev")
-    if(Math.floor(maxIndex) > currentIndex){
+    if (Math.floor(maxIndex) > currentIndex) {
         nextBtn.classList.add("act")
     }
-    else{
+    else {
         nextBtn.classList.remove("act")
     }
 
 
-    if(currentIndex > 1){
+    if (currentIndex > 1) {
         prevBtn.classList.add("act")
     }
 })
 
 
-function next(){
+function next() {
     var prevBtn = document.querySelector('.prev')
     var nextBtn = document.querySelector('.next')
 
-    if(currentIndex <= maxIndex - 1){
+    if (currentIndex <= maxIndex - 1) {
         currentIndex++
         prevBtn.classList.add("act")
 
         highlightIndexBtn()
     }
 
-    if(currentIndex > maxIndex - 1){
+    if (currentIndex > maxIndex - 1) {
         nextBtn.classList.remove("act")
     }
 }
 
 
-function prev(){
+function prev() {
     var prevBtn = document.querySelector('.prev')
 
-    if(currentIndex > 1){
+    if (currentIndex > 1) {
         currentIndex--
         prevBtn.classList.add("act")
         highlightIndexBtn()
     }
 
-    if(currentIndex < 2){
+    if (currentIndex < 2) {
         prevBtn.classList.remove("act")
     }
 }
 
 
-function paginationBtn(i){
+function paginationBtn(i) {
     currentIndex = i
 
     var prevBtn = document.querySelector('.prev')
@@ -442,26 +442,26 @@ function paginationBtn(i){
 
     highlightIndexBtn()
 
-    if(currentIndex > maxIndex - 1){
+    if (currentIndex > maxIndex - 1) {
         nextBtn.classList.remove('act')
     }
-    else{
+    else {
         nextBtn.classList.add("act")
     }
 
 
-    if(currentIndex > 1){
+    if (currentIndex > 1) {
         prevBtn.classList.add("act")
     }
 
-    if(currentIndex < 2){
+    if (currentIndex < 2) {
         prevBtn.classList.remove("act")
     }
 }
 
 
 // Editar registro acción de tabla
-tabSize.addEventListener('change', ()=>{
+tabSize.addEventListener('change', () => {
     var selectedValue = parseInt(tabSize.value)
     tableSize = selectedValue
     currentIndex = 1
